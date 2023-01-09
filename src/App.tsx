8972732f3e88ebac1193745f94e9b93ea8cfddcc
David Kang <davidkang@Davids-MacBook-Pro.local>
2023-01-09 09:37:44 -0800

    commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { DraftPage } from './components/components/DraftPage';
+
+function App() {
+  return (
+    <div className="App">
+      <DraftPage />
+    </div>
+  );
+}
+
+export default App;
